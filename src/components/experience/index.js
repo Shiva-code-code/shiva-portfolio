06e758228cdc29907ff3bd462821d961271e1bd3
Shiva Kumar Hazari <hshiva7661016@gmail.com>
2025-06-21 16:23:48 +0530

    Updated Experience section with Google OSS contribution

--- src/components/experience/index.js
+++ src/components/experience/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import hc from '../../images/logos/Hc.jpg';
-import es from '../../images/logos/es.png';
-import fp from '../../images/logos/fp.jpg';
+import googleLogo from '../../images/logos/google.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
 class Experience extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
     renderExperienceCard(logo, title, period, location, role, description) {
         return (
             <div className="col-lg-8 col-md-10 col-sm-12 mt-3">
@@ -36,46 +29,26 @@ class Experience extends React.Component {
             </div>
         );
     }
-    
+
     render() {
         return (
-            <>
-                <div className="container mt-5" id='experience'>
-                    <div className="row justify-content-center">
-                        <div className='col-12 text-center'>
-                            {/* <h2>Experience</h2> */}
-                        </div>
-
-                        {/* {this.renderExperienceCard(
-                            hc, 
-                            "Health Compass Private Limited", 
-                            "<b>From:</b> 15/02/2023 &nbsp; <b>to:</b> 14/08/2023", 
-                            "Gurgaon", 
-                            "Remote",
-                            "Managed React JS frontend and Django backend, enhancing app efficiency by 30%, transitioned backend from Spring Boot/Node.js to Django, boosting system performance by 45%."
-                        )} */}
-                        
-                        {/* {this.renderExperienceCard(
-                            fp, 
-                            "Foruppo", 
-                            "<b>From:</b> 02/12/2022 &nbsp; <b>to:</b> 14/02/2023", 
-                            "Banglore", 
-                            "Remote",
-                            "Oversaw project timelines and team communication as a program coordinator intern, improving project delivery efficiency by 15%, managed client interactions, aligning project goals with client needs, resulting in a 20% rise in client satisfaction."
-                        )} */}
-                        
-                        {/* {this.renderExperienceCard(
-                            es, 
-                            "Espark Technologies", 
-                            "<b>From:</b> 15/03/2021 &nbsp; <b>to:</b> 15/05/2023", 
-                            "Hyderabad", 
-                            "On Site",
-                            "Contributed to designing and developing embedded systems, resulting in a 20% reduction in prototype development time, aided in creating prototypes for new products, streamlining the innovation process by 25% for system improvements"
-                        )} */}
+            <div className="container mt-5" id='experience'>
+                <div className="row justify-content-center">
+                    <div className='col-12 text-center'>
+                        <h2>Experience</h2>
                     </div>
+
+                    {this.renderExperienceCard(
+                        googleLogo,
+                        "Google (Open Source)",
+                        "<b>From:</b> Apr 2025 &nbsp; <b>to:</b> Present",
+                        "Remote",
+                        "Contributor",
+                        "Contributing to Google’s fhir-data-pipes project. Migrated legacy unit tests from JUnit 4 to JUnit 5. Collaborating with Google engineers via GitHub PRs and CI pipelines using Maven and GitHub Actions."
+                    )}
                 </div>
-            </>
-        )
+            </div>
+        );
     }
 }
 
